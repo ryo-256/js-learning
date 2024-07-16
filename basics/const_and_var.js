61e7dd36bb9d6aabd7b1305deb71132eeb03daa3
RyoNonoyama256 <119028353+RyoNonoyama256@users.noreply.github.com>
2024-07-16 23:34:38 +0900

    learned const, var, and templateString 20240716

---
+++ basics/const_and_var.js
@@ -0,0 +1,13 @@
+let year = 2023
+year = year + 1
+console.log(year)
+// output: 2024
+
+const year_const = 2023
+// const year_const = 2024
+/* output:
+    Uncaught SyntaxError: Identifier 'year_const' has already been declared
+*/
+
+
+
---
+++ basics/template_strings.js
@@ -0,0 +1,10 @@
+const age = 25
+console.log("I'm " + age + " years old.")
+console.log('I\'m ' + age + " years old.")
+console.log(`I'm ${age} years old.`)
+
+/* output: 
+    I'm 25 years old.
+    I'm 25 years old.
+    I'm 25 years old.
+*/
